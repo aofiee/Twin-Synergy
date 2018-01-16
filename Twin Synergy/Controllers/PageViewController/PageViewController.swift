90db0a5654f2bafb326c060bf9867459e9da76e8
Khomkrid Lerdprasert <khomkrid@twinsynergy.co.th>
2018-01-16 16:37:32 +0700

    Change HomeViewController to UIPageViewController

---
+++ Twin Synergy/Controllers/PageViewController/PageViewController.swift
@@ -0,0 +1,83 @@
+//
+//  PageViewController.swift
+//  Twin Synergy
+//
+//  Created by SkullTree on 16/1/2561 BE.
+//  Copyright © 2561 SkullTree. All rights reserved.
+//
+
+import UIKit
+
+class PageViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.dataSource = self
+        if let firstViewController = orderedViewControllers.first {
+            setViewControllers([firstViewController],
+                               direction: .forward,
+                               animated: true,
+                               completion: nil)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    private(set) lazy var orderedViewControllers: [UIViewController] = {
+        return [
+            self.newColoredViewController(viewName: "HomeViewController"),
+            self.newColoredViewController(viewName: "WebSiteViewController"),
+            self.newColoredViewController(viewName: "HomeViewController")
+        ]
+    }()
+    private func newColoredViewController(viewName: String) -> UIViewController {
+        return UIStoryboard(name: "Main", bundle: nil) .
+            instantiateViewController(withIdentifier: viewName)
+    }
+}
+
+extension PageViewController: UIPageViewControllerDataSource {
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        // User is on the first view controller and swiped left to loop to
+        // the last view controller.
+        guard previousIndex >= 0 else {
+            return orderedViewControllers.last
+        }
+        
+        guard orderedViewControllers.count > previousIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[previousIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        let orderedViewControllersCount = orderedViewControllers.count
+        
+        // User is on the last view controller and swiped right to loop to
+        // the first view controller.
+        guard orderedViewControllersCount != nextIndex else {
+            return orderedViewControllers.first
+        }
+        
+        guard orderedViewControllersCount > nextIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[nextIndex]
+    }
+}
---
+++ Twin Synergy/Controllers/WebSiteViewController/WebSiteViewController.swift
@@ -0,0 +1,67 @@
+//
+//  WebSiteViewController.swift
+//  Twin Synergy
+//
+//  Created by SkullTree on 16/1/2561 BE.
+//  Copyright © 2561 SkullTree. All rights reserved.
+//
+
+import UIKit
+
+class WebSiteViewController: CustomNavigationController {
+    @IBOutlet var myTab: UITableView!
+    fileprivate let itemsPerRow: CGFloat = 3
+    fileprivate let sectionInsets = UIEdgeInsets(top: 20.0, left: 10.0, bottom: 20.0, right: 10.0)
+    private var mobileListViewModel: MobileViewModel!
+    private var webListViewModel: WebViewModel!
+    private var dataAccess :DataAccess!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.dataAccess = DataAccess()
+        self.mobileListViewModel = MobileViewModel(dataAccess: self.dataAccess)
+        self.webListViewModel = WebViewModel(dataAccess: self.dataAccess)
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
+extension WebSiteViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.webListViewModel.webViewModels.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "mCell", for: indexPath) as! homeTableViewCell
+        cell.coverImageView.image = UIImage.init(named: self.webListViewModel.webViewModels[indexPath.row].webCover)
+        cell.titleLabel.text = self.webListViewModel.webViewModels[indexPath.row].webTitle
+        cell.descLabel.text = self.webListViewModel.webViewModels[indexPath.row].webDescription
+        return cell
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat
+    {
+        return 304;
+    }
+}
+
+extension WebSiteViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        print(self.mobileListViewModel.mobileViewModels.count)
+        return self.mobileListViewModel.mobileViewModels.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "homeMobileCell", for: indexPath) as! homeCollectionViewCell
+        cell.coverImage.image = UIImage.init(named: self.mobileListViewModel.mobileViewModels[indexPath.item].mobileCover)
+        cell.titleHeader.text = self.mobileListViewModel.mobileViewModels[indexPath.item].mobileTitle
+        cell.desc.text = self.mobileListViewModel.mobileViewModels[indexPath.item].mobileDescription
+        return cell
+    }
+    
+}
